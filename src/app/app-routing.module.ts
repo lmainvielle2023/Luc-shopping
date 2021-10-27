ff26301bb07a52befbb90a7eb923ba377d2fafa1
Luc Mainvielle <lmainvielle2023@chadwickschool.org>
2021-10-27 11:37:38 -0700

    feat: create new project with routing. Set up checkout page routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { cartCheckout } from './components/cart-checkout/cart-checkout.component';
+import { HomeComponent } from './components/shared/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'cart-checkout', component: cartCheckout},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/cart-checkout/cart-checkout.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: 'app-cart-checkout',
+  templateUrl: './cart-checkout.component.html',
+  styleUrls: ['./cart-checkout.component.css']
+})
+
+export class cartCheckout implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
